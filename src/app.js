5e0ee95d3e3b45abcf0b5c9192ec23e271de7b68
Sachin Parmar <parmarsachin707@gmail.com>
2025-09-22 00:20:04 +0530

    middleware setup

--- src/app.js
+++ src/app.js
@@ -1,8 +1,26 @@
+import logger from '#config/logger.js';
 import express from 'express';
+import helmet from "helmet"; //Help secure Express apps with various HTTP headers
+import morgan from 'morgan'; // help to log the req time, payload, and others
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(
+  morgan('combined', {
+    stream: { write: message => logger.info(message.trim()) },
+  })
+);
+
 app.get('/', (req, res) => {
+  logger.info('Hello from Acquisitions!');
   res.status(200).send('Hello from Acquisitions!');
 });
 
